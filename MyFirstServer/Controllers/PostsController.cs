47d3db846bd7de7c1535bd58b0e2202997ba0486
w1nk1y <101086274+w1nk1y@users.noreply.github.com>
2024-07-02 13:21:54 +0600

    Add files via upload

---
+++ MyFirstServer/Controllers/PostsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstServer.Models;
+
+namespace LiveServer.Controllers;
+
+[ApiController]
+[Route(template: "api/[controller]")]
+public class PostsController : ControllerBase
+{
+    private const string FilePath = "posts.json";
+    private new readonly JsonSerializerOptions _options = new() { WriteIndented = true, PropertyNameCaseInsensitive = true };
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        if (!System.IO.File.Exists(FilePath))
+            return NotFound();
+
+        string posts = System.IO.File.ReadAllText(FilePath);
+        return Ok(posts);
+    }
+    [HttpPost]
+    public IActionResult CreatePost([FromBody] Post post)
+    {
+        if (!System.IO.File.Exists(FilePath))
+            return NotFound();
+
+        string postsJson = System.IO.File.ReadAllText(FilePath);
+        List<Post> posts = JsonSerializer.Deserialize < List < Post>>(postsJson, _options)!;
+        posts.Add(post);
+
+        string postJson = JsonSerializer.Serialize(posts, _options);
+        System.IO.File.WriteAllText(FilePath, postJson);
+
+        return Ok(post);
+
+    }
+    [HttpGet(template: "{id:int}")]
+    public IActionResult GetById(int id)
+    {
+
+        string postsJson = System.IO.File.ReadAllText(FilePath);
+        List<Post> posts = JsonSerializer.Deserialize<List<Post>>(postsJson, _options)!;
+        Post? post = posts.SingleOrDefault(p => p.Id == id);
+
+        if (post is null) return NotFound();
+
+        return Ok(post);
+    }
+
+}
---
+++ MyFirstServer/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapControllers();
+
+app.UseCors("AllowAll");
+
+app.Run();
