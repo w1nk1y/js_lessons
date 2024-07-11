2f549c6b79717817fcaea2c8e15af1cecfb1966c
w1nk1y <101086274+w1nk1y@users.noreply.github.com>
2024-07-11 15:36:13 +0600

    Add files via upload

---
+++ frontend/src/components/PostModel.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <router-link :to="{ name: 'show', params: { id: post.id } }">
+
+        
+        <p class="postnumb">Пост номер {{ post.id }}</p>
+        <h2 v-if="$route.path === '/'" >{{  post.title }}</h2>
+        
+        <h2 v-else >{{ post.title }}</h2>
+
+        <p style="display:inline-block;width:1000px;" v-if="$route.path === '/'"  > {{ post.body}}</p>
+        <p v-else >{{post.body }}</p>
+         
+    </router-link>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: "postComponent",
+  props: {
+    post:{},
+  },
+  computed:{
+    postID(){
+        return this.post.id;
+  }
+}
+};
+</script>
+
+  
+<style>
+ 
+    div{  
+             
+        margin-bottom: 30px;
+        padding: 20px;
+        box-sizing: border-box;
+    }
+    h2{
+        margin: 0 0 10px;
+        font-size: 18px;
+        font-weight: bold;
+        color: #333; 
+    }
+    p{
+        margin: 0;
+        font-size: 16px;
+        color: #666;  
+    }
+    .postnumb{
+        color:#4CAF50
+    }
+</style>
