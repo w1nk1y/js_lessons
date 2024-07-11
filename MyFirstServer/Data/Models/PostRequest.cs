7258c029a70328723ae81c8f854a47bba080684c
w1nk1y <101086274+w1nk1y@users.noreply.github.com>
2024-07-11 15:35:05 +0600

    Delete MyFirstServer directory

--- MyFirstServer/Data/Models/PostRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyFirstServer.Data.Models;
-public class PostRequest
-{
-    public string title { get; set; }
-    public string body { get; set; }
-    public int userId { get; set; }
-}
--- MyFirstServer/Data/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyFirstServer.Data.Models;
-public class User
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-    public IEnumerable<Post> Posts { get; set; }
-}
